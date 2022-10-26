b010ed51121f087e985bf1b9a5e68df9dd52cf2b
lpch0411 <lai00055@algonquinlive.com>
2022-10-26 11:21:22 -0400

    fixed Dices

--- assets/components/Dices.js
+++ assets/components/Dices.js
@@ -1,72 +1,56 @@
+let rollcounter = 3;
+
 let images = ["../dice-1.png",
 "../dice-2.png",
 "../dice-3.png",
 "../dice-4.png",
 "../dice-5.png",
 "../dice-6.png"];
-let dice = document.querySelectorAll("img");
 
-document.querySelector("#die-1").setAttribute("src", images[0]);
-document.querySelector("#die-2").setAttribute("src", images[0]);
-document.querySelector("#die-3").setAttribute("src", images[0]);
-document.querySelector("#die-4").setAttribute("src", images[0]);
-document.querySelector("#die-5").setAttribute("src", images[0]);
-       
-function roll(){
-    dice.forEach(function(die){
-        die.classList.add("shake");
-    });
-    setTimeout(function(){
-    dice.forEach(function(die){
-        die.classList.remove("shake");
-    });
-    const obj1 = {value: Math.floor(Math.random()*6) +1};
-    const obj2 = {value: Math.floor(Math.random()*6) +1};
-    const obj3 = {value: Math.floor(Math.random()*6) +1};
-    const obj4 = {value: Math.floor(Math.random()*6) +1};
-    const obj5 = {value: Math.floor(Math.random()*6) +1};
 
+
+const dice = [
+
+   {value: 1, hold: false, image: document.querySelector("#die1") },
+   {value: 1, hold: false, image: document.querySelector("#die2")},
+  {value: 1, hold: false, image: document.querySelector("#die3")},
+   {value: 1, hold: false, image: document.querySelector("#die4")},
+   {value: 1, hold: false, image: document.querySelector("#die5")}
+];
+     
+dice.forEach(function(die){
+		
+   die.image.setAttribute("src", images[die.value -1]);
+   die.image.addEventListener("click",function handleClick(event) {
+	   if (rollcounter != 0) {
+    event.target.classList.toggle('hold');
+	die.hold = !die.hold;
+	console.log(die.value, die.hold);
+	   };
+    });
+
+	  });
+	  
+function rolldice(){
+    dice.forEach(function(die){
+        if (!die.hold) {
+		die.image.classList.add("shake");
+		die.value = Math.floor(Math.random()*6) +1;		};
+    });
+    setTimeout(function(){ 
+    dice.forEach(function(die){
+		if (!die.hold) {
+        die.image.classList.remove("shake"); };
+    });
+	
+	  dice.forEach(function(die){
+		if (!die.hold) {
     
-    let valuearray = [obj1.value,obj2.value,obj3.value,obj4.value,obj5.value]
-    
-    console.log(obj1.value,obj2.value,obj3.value,obj4.value,obj5.value);
-    document.querySelector("#die-1").setAttribute("src", images[valuearray[0] -1]);
-    document.querySelector("#die-2").setAttribute("src", images[valuearray[1] -1]);
-    document.querySelector("#die-3").setAttribute("src", images[valuearray[2] -1]);
-    document.querySelector("#die-4").setAttribute("src", images[valuearray[3] -1]);
-    document.querySelector("#die-5").setAttribute("src", images[valuearray[4] -1]);
+   die.image.setAttribute("src", images[die.value -1]);
+	  };
+	  });
   
 },
     1000
     )
 };
-
-//click to create shadow for each cube
-let die1 = document.querySelector("#die-1");
-die1.addEventListener("click",function handleClick(event) {
-    event.target.classList.toggle('hold');
-    });
-let die2 = document.querySelector("#die-2");
-die2.addEventListener("click",function handleClick(event) {
-      event.target.classList.toggle('hold');
-    });
-let die3 = document.querySelector("#die-3");
-die3.addEventListener("click",function handleClick(event) {
-      event.target.classList.toggle('hold');
-    });
-let die4 = document.querySelector("#die-4");
-die4.addEventListener("click",function handleClick(event) {
-      event.target.classList.toggle('hold');
-    });
-let die5 = document.querySelector("#die-5");
-die5.addEventListener("click",function handleClick(event) {
-      event.target.classList.toggle('hold');
-    });
-
-let objarray= [die1,die2,die3,die4,die5];
-
-//still incomplete function
-function hold(){
-    Object.seal(objarray[1]);
-};
-hold();
